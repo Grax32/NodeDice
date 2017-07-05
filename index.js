40d63074045baf9e1e797503e076982865e9497b
David Walker <david.walker@agilethought.com>
2017-07-05 16:31:51 -0400

    add gruntfile contents

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+    response.end("Hello There!");
 
 });
 
+var host = "localhost";
 var port = process.env.PORT || 1337;
-server.listen(port);
+
+server.listen({ host: host, port: port});
 
 console.log("Server running at http://localhost:%d", port);
+
