7cba353ae7d334eb0964948ae5aeddaf85dcb36d
David Walker <david.walker@agilethought.com>
2017-07-06 16:03:35 -0400

    update

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 var http = require('http');
 
 var server = http.createServer(function(request, response) {
-
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello Wonderful People!");
 
 });
 
 var port = process.env.PORT || 1337;
-console.error("Server running at http://%s:%d", sitename, port);
+// console.error("Server running at http://%s:%d", sitename, port);
 
 server.listen(port);
 
