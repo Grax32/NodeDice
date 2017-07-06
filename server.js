f629aab8edf52611555d63bb25e0fd0eae3471c7
David Walker <david.walker@agilethought.com>
2017-07-06 17:04:41 -0400

    ops

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"});
 	response.write("Greetings.");
+	response.write("Greetings.");
+	response.write("Greetings.");
     response.end("Hello Wonderful People!");
 });
 
