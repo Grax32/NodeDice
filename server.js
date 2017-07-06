8359a2ccbc0bb8f91e64a2fb66e9891a39e0d981
David Walker <david.walker@agilethought.com>
2017-07-06 11:22:05 -0400

    update port settings

--- server.js
+++ server.js
@@ -7,12 +7,10 @@ var server = http.createServer(function(request, response) {
 
 });
 
-var port = process.env.PORT || 1337;
-var settings = { port: port };
-
-server.listen(settings);
+var port = 80;
+server.listen({ port: port });
 
 var sitename = process.env.WEBSITE_SITE_NAME;
 
-console.log("Server running at http://%s:%d",sitename, port);
+console.log("Server running at http://%s:%d", sitename, port);
 
