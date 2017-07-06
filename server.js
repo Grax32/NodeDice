caf8fe1997b560fc3394e5777091aee26dfc3f8c
David Walker <david.walker@agilethought.com>
2017-07-06 16:53:44 -0400

    stuff n things

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var http = require('http');
 
 var server = http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write("Greetings.");
     response.end("Hello Wonderful People!");
 });
 
