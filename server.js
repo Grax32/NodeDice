20c65b05000cf29c2b663d11b8daa4893af171f2
David Walker <david.walker@agilethought.com>
2017-07-06 11:12:21 -0400

    add server.js to git

---
+++ server.js
@@ -0,0 +1,18 @@
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello Wonderful People!");
+
+});
+
+var port = process.env.PORT || 1337;
+var settings = { port: port };
+
+server.listen(settings);
+
+var sitename = process.env.WEBSITE_SITE_NAME;
+
+console.log("Server running at http://%s:%d",sitename, port);
+
