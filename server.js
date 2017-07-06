53e14685491a66bf3bcb97db6f53947129c6954d
David Walker <david.walker@agilethought.com>
2017-07-06 18:31:04 -0400

    fix multiline strings

--- server.js
+++ server.js
@@ -6,10 +6,7 @@ var serverToken = process.env.APP_SERVER_TOKEN || "UNKNOWN";
 
 var server = http.createServer(function(request, response) {
     response.setHeader('Content-Type', 'text/plain; charset=UTF-8');
-    response.end("Hello Wonderful People!
-The serverURL is " + serverURL + "
-The serverToken is " + serverToken + "
-	");
+    response.end("Hello Wonderful People!\nThe serverURL is " + serverURL + "\nThe serverToken is " + serverToken + "\n");
 });
 
 var port = process.env.PORT || 1337;
