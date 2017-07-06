f07c9712fc074c75d048e0f68d8b9cc153cca667
David Walker <david.walker@agilethought.com>
2017-07-06 16:09:01 -0400

    yup

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello Wonderful People!");
-
 });
 
 var port = process.env.PORT || 1337;
